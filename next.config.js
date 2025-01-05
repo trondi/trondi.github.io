0e90b43c1ced2ac49b6618dbec0561e2ed81eb8e
trondi <trond746@gmail.com>
2025-01-05 19:43:19 +0900

    :sparkles: github.io setting

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+const prefix =
+    process.env.NODE_ENV === 'production' ? 'https://trondi.github.io/test/' : ''
+
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  assetPrefix: prefix,
 }
 
 module.exports = nextConfig
