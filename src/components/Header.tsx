aeea6c0a4ef3cb0d72755c9e348162d1c3446de5
trondi <trond746@gmail.com>
2025-02-22 23:30:12 +0900

    fix: git action 에러

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,21 +14,24 @@ import {
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import Link from "next/link";
 
-function Header(props) {
+function Header() {
   return (
     <header className="flex justify-center items-center h-16 bg-gray-800 text-white">
       <nav className="flex items-center justify-end w-full max-w-7xl p-4 mx-auto">
         <ul className="flex items-center space-x-4">
           <li className="flex items-center">
-            <h1 className="flex items-center leading-none" href="/">
-              Home
-            </h1>
+            <Link href={"/"}>
+              <h1 className="flex items-center leading-none">Home</h1>
+            </Link>
           </li>
           <li className="flex items-center">
-            <a className="flex items-center leading-none" href="/about">
-              About
-            </a>
+            <Link href={"/about"}>
+              <a className="flex items-center leading-none" href="/about">
+                About
+              </a>
+            </Link>
           </li>
           <li className="flex items-center hover:text-blue-500">
             <div className="flex items-center space-x-2">
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { persistor, store, wrapper } from "@store/index";
 import Layout from "@components/Layout";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+import Header from "@components/Header";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
