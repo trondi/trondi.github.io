a474ab74c91e308ce069b60ccb66a4f79ef04968
trondi <trond746@gmail.com>
2025-03-16 23:56:24 +0900

    :sparkles: feat: carousel 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,15 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import * as React from "react";
+
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,6 +19,28 @@ export default function Home() {
       className={`flex flex-col items-center justify-between p-24 ${inter.className}`}
     >
       <div className={`border-2 border-amber-300 w-full `}>Banner</div>
+
+      <Carousel className="w-full max-w-sm">
+        <CarouselContent className="-ml-1">
+          {Array.from({ length: 5 }).map((_, index) => (
+            <CarouselItem
+              key={index}
+              className="pl-1 md:basis-1/2 lg:basis-1/3"
+            >
+              <div className="p-1">
+                <Card>
+                  <CardContent className="flex aspect-square items-center justify-center p-6">
+                    <span className="text-2xl font-semibold">{index + 1}</span>
+                  </CardContent>
+                </Card>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+
       <div className={`border-2 border-amber-300 w-full `}>Category</div>
       <div className={`border-2 border-amber-300 w-full `}>Contents</div>
     </main>
