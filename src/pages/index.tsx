9716c0315a64b8bd96805a513adb271e22a7eb91
trondi <trond746@gmail.com>
2025-03-27 23:48:49 +0900

    feat: carousel 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,19 +18,14 @@ export default function Home() {
     <main
       className={`flex flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <div className={`border-2 border-amber-300 w-full `}>Banner</div>
-
-      <Carousel className="w-full max-w-sm">
-        <CarouselContent className="-ml-1">
+      <Carousel className="w-full max-w-xs">
+        <CarouselContent>
           {Array.from({ length: 5 }).map((_, index) => (
-            <CarouselItem
-              key={index}
-              className="pl-1 md:basis-1/2 lg:basis-1/3"
-            >
+            <CarouselItem key={index}>
               <div className="p-1">
                 <Card>
                   <CardContent className="flex aspect-square items-center justify-center p-6">
-                    <span className="text-2xl font-semibold">{index + 1}</span>
+                    <span className="text-4xl font-semibold">{index + 1}</span>
                   </CardContent>
                 </Card>
               </div>
@@ -40,7 +35,6 @@ export default function Home() {
         <CarouselPrevious />
         <CarouselNext />
       </Carousel>
-
       <div className={`border-2 border-amber-300 w-full `}>Category</div>
       <div className={`border-2 border-amber-300 w-full `}>Contents</div>
     </main>
